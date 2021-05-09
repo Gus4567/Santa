c31c19191355eb89829c317405a88205afaff71e
mardock12 <mardock12@users.noreply.github.com>
2021-05-08 21:34:18 -0300

    New function to parser the date

--- src/utils/parsedate.js
+++ src/utils/parsedate.js
@@ -1,5 +1,9 @@
-const moment= require('moment')
+//transform the date in format Epoch into Unix for match in the API response object
 
-const parse= (date => {
-    date
-}) 
+const newDate = (date) => {
+  return Math.round(new Date(date) / 1000.0);
+};
+
+module.exports = {
+  newDate: newDate,
+};
