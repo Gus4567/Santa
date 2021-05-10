ad96826238029cafa4a2bb7d753bef72e0dc5a70
mardock12 <mardock12@users.noreply.github.com>
2021-05-09 21:27:35 -0300

    how many beers endpoint

--- src/routes/howmany.js
+++ src/routes/howmany.js
@@ -1,19 +1,38 @@
-const express= require('express')
-const router= express.Router()
-const Meetmodel= require("../models/meets")
+const express = require("express");
+const parseDate = require("../utils/parsedate");
+const router = express.Router();
+const Meetmodel = require("../models/meets");
+const Usermodel = require("../models/users");
+const getTemp = require("../utils/weather");
+const howmany= require('../utils/beers')
 
-router.get('/how-many-beers', async (req, res)=>{
-    try {
-        const {name}= req.body
-        const meet= await Meetmodel.find({name})
-        const amount= meet.people
-        console.log(amount)
-        console.log(meet)
-        res.send(meet)
-    } 
-    catch(e){
-        res.status(500)
+//required beers function
+const beers = require("../utils/beers");
+
+router.get("/how-many-beers", async (req, res) => {
+  const { name, meet_name } = req.query
+  console.log(name, meet_name)
+  
+  try {
+    const meet = await Meetmodel.find({ name });
+    const user= await Usermodel.find({isadmin}) 
+
+    if (meet.length > 0 && user.isadmin) {
+      const amountPeople = meet[0].people;
+      const date = meet[0].date;
+      const dateEpoch = await parseDate.newDate(date);
+      const temp = await getTemp.getWeather(dateEpoch);
+      
+      const howMany= howmany.howmany(amountPeople, temp)
+      console.log(howMany)
+      res.status(200).send(meet);
+    } else {
+      res.status(400).send({ message: `not meets find with name ${name}` });
     }
-    
-})
-module.exports= router
+  } catch (e) {
+    console.log(e)
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
