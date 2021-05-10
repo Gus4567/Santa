2750f5b88f471e6b391212499035ca274381ec68
mardock12 <mardock12@users.noreply.github.com>
2021-05-10 15:27:50 -0300

    features: readme

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { rolePermissions } = require("../middlewares");
 const { meetsController, tempsController } = require("../controllers");
+
 router.use(rolePermissions);
 router.get("/meet/beers", meetsController);
 router.get("/meet/temp", tempsController);
+router.post("meet/create");
 module.exports = router;
